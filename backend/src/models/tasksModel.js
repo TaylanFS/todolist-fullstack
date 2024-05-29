06f8de2189d60fa0295cf04ef3d8c766d82859cc
Taylan <ferreirataylan123@gmail.com>
2024-05-29 17:37:11 -0300

    added models to the project

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,6 +1,9 @@
-const { get } = require("../router");
+const connection = require("./connection");
 
-const getAll = () => {};
+const getAll = async () => {
+	const tasks = await connection.execute("SELECT * FROM tasks");
+	return tasks;
+};
 
 module.exports = {
 	getAll,
